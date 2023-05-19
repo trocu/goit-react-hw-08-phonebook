70f92c730520c59b7a8c1de9c1808e4525cc97fa
Mariusz Trocewicz <throcu@gmail.com>
2023-05-20 01:05:40 +0200

    Added form with name input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,62 @@
 import "./App.css";
+import { Component } from "react";
+import { nanoid } from "nanoid";
 
-function App() {}
+export default class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
 
-export default App;
+  handleChange = e => {
+    e.preventDefault();
+    // console.log("e.target.value: ", e.target.value);
+    // console.log("e.currentTarget: ", e.currentTarget);
+
+    // const name = e.target.value;
+    this.setState({ name: e.target.value, id: nanoid() });
+  };
+
+  //   handleSubmit = e => {
+  //     e.preventDefault();
+  //     // console.log("e: ", e);
+  //     // console.log("e.target.name.value: ", e.target.name.value);
+  //     const form = e.currentTarget;
+  //     const name = form.elements.name.value;
+  //     // console.log("name: ", name);
+  //     this.setState({ name: e.target.value });
+  //     console.log("e.target.value: ", e.target.value);
+  //     console.log("this state: ", this.state);
+  //     // this.props.onSubmit({ name });
+  //   };
+
+  render() {
+    const { name } = this.state;
+    console.log(this.state);
+    return (
+      <>
+        <div>
+          <p>Phonebook</p>
+          <form>
+            <label>
+              <span>Name</span>
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.handleChange}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </div>
+        <div>
+          <p>Contacts</p>
+        </div>
+      </>
+    );
+  }
+}
