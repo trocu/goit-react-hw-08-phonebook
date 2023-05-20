474cc575d110f1c2a1c2c777668f89329c0dd61a
Mariusz Trocewicz <throcu@gmail.com>
2023-05-20 20:08:41 +0200

    Submit button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Component } from "react";
 import { nanoid } from "nanoid";
+// import debounce from "lodash/debounce";
 
 export default class App extends Component {
   state = {
@@ -14,36 +15,31 @@ export default class App extends Component {
     // console.log("e.currentTarget: ", e.currentTarget);
 
     // const name = e.target.value;
-    this.setState({ name: e.target.value, id: nanoid() });
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+    // this.setState({ name: value, id: nanoid() });
   };
 
-  //   handleSubmit = e => {
-  //     e.preventDefault();
-  //     // console.log("e: ", e);
-  //     // console.log("e.target.name.value: ", e.target.name.value);
-  //     const form = e.currentTarget;
-  //     const name = form.elements.name.value;
-  //     // console.log("name: ", name);
-  //     this.setState({ name: e.target.value });
-  //     console.log("e.target.value: ", e.target.value);
-  //     console.log("this state: ", this.state);
-  //     // this.props.onSubmit({ name });
-  //   };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("this state name: ", this.state.name);
+    this.reset();
+  };
 
   render() {
-    const { name } = this.state;
+    const { person } = this.state;
     console.log(this.state);
     return (
       <>
         <div>
           <p>Phonebook</p>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <label>
               <span>Name</span>
               <input
                 type="text"
                 name="name"
-                value={name}
+                value={person}
                 onChange={this.handleChange}
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -53,9 +49,12 @@ export default class App extends Component {
             <button type="submit">Add contact</button>
           </form>
         </div>
-        <div>
+        <section>
           <p>Contacts</p>
-        </div>
+          <ul>
+            <li></li>
+          </ul>
+        </section>
       </>
     );
   }
