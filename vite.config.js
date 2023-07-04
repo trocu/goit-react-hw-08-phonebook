ba6cd47e494788ba4194aa399803c96da1789e8c
Mariusz Trocewicz <throcu@gmail.com>
2023-07-05 00:02:08 +0200

    Changed base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/goit-react-hw-07-phonebook',
+  base: '/goit-react-hw-08-phonebook',
 });
