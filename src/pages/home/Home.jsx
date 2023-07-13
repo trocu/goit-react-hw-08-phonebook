c2136141620f4d702bfd3ba2af053d580d36f854
Mariusz Trocewicz <throcu@gmail.com>
2023-07-13 02:33:39 +0200

    Added phone img

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,69 +1,85 @@
-import { Avatar, Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { Avatar, Box, Flex, Grid, GridItem, Heading, Img, Text } from '@chakra-ui/react';
 import bell from '../../assets/bell.webp';
+import phone from '../../assets/phone.webp';
 
 const Home = () => {
   return (
     <Box
       as='main'
       height='100vh'
-      p={15}
+      pl={15}
       maxW='32rem'
     >
-      <Heading
-        as='h1'
-        size='3xl'
-        color='red.500'
-        fontWeight='bold'
-        textAlign='left'
-        mt='10'
+      <Grid
+        h='100vh'
+        w='100vw'
+        templateColumns='repeat(2, 1fr)'
+        gap={4}
       >
-        Phonebook
-      </Heading>
-      <Heading
-        as='h2'
-        size='2xl'
-        color='gray.700'
-        textAlign='left'
-      >
-        Your personal contact database
-      </Heading>
-      <Box mt='10'>
-        <Flex spacing='4'>
-          <Flex
-            gap='4'
-            alignItems='center'
-            flexWrap='wrap'
+        <GridItem
+          rowSpan={1}
+          colSpan={1}
+        >
+          <Heading
+            as='h1'
+            size='3xl'
+            color='red.500'
+            fontWeight='bold'
+            textAlign='left'
+            mt='10'
           >
-            <Avatar
-              size='lg'
-              name='Segun Adebayo'
-              src={bell}
-            />
+            Phonebook
+          </Heading>
+          <Heading
+            as='h2'
+            size='2xl'
+            color='gray.700'
+            textAlign='left'
+          >
+            Your personal contact database
+          </Heading>
+          <Box mt='10'>
+            <Flex spacing='4'>
+              <Flex
+                gap='4'
+                alignItems='center'
+                flexWrap='wrap'
+              >
+                <Avatar
+                  size='lg'
+                  name='Segun Adebayo'
+                  src={bell}
+                />
 
-            <Box>
-              <Heading
-                size='sm'
-                as='i'
-                color='gray.600'
-              >
-                “Mr. Watson – Come here – I want to see you.”
-              </Heading>
-              <Text
-                fontSize='sm'
-                color='gray.500'
-              >
-                [First intelligible words spoken over the telephone]
-              </Text>
-              <Text
-                color='red.500'
-                fontWeight='semibold'
-              >
-                - Alexander Graham Bell
-              </Text>
-            </Box>
-          </Flex>
-        </Flex>
-      </Box>
+                <Box>
+                  <Heading
+                    size='sm'
+                    as='i'
+                    color='gray.600'
+                  >
+                    “Mr. Watson – Come here – I want to see you.”
+                  </Heading>
+                  <Text
+                    fontSize='sm'
+                    color='gray.500'
+                  >
+                    [First intelligible words spoken over the telephone]
+                  </Text>
+                  <Text
+                    color='red.500'
+                    fontWeight='semibold'
+                  >
+                    - Alexander Graham Bell
+                  </Text>
+                </Box>
+              </Flex>
+            </Flex>
+          </Box>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Img src={phone} />
+        </GridItem>
+      </Grid>
     </Box>
   );
 };
